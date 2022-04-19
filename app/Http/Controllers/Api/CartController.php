28de0a46e5310d7007d313bdc1b5b023514e6d02
sabreenMohammed107 <sabreenm312@gmail.com>
2022-04-19 17:46:08 +0200

    testing

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -5,8 +5,11 @@ namespace App\Http\Controllers\Api;
 use App\Http\Resources\OrderItemsResource;
 use App\Models\Order;
 use App\Models\Order_item;
+use Carbon\Carbon;
 use GuzzleHttp\Psr7\Request;
 use Illuminate\Support\Facades\Auth;
+use Validator;
+use Illuminate\Support\Facades\DB;
 
 class CartController extends BaseController
 {
@@ -20,7 +23,7 @@ class CartController extends BaseController
             if ($myCart) {
                 $items = Order_item::where('order_id', $myCart->id)->get();
                 return $this->sendResponse(OrderItemsResource::collection($items), 'get all Items');
-            }else{
+            } else {
                 return $this->sendError('some thing error in Cart');
             }
 
@@ -30,7 +33,63 @@ class CartController extends BaseController
         }
     }
 
+    public function addItem(Request $request)
+    {
+        $validator =Validator::make($request->all(), [
+            'item_id' => 'required',
 
+        ]);
+//exist product
+        $user = Auth::user();
+        $exist = Order::where('status', "=", 0)->where('user_id', $user->id)->first();
+        if ($exist) {
+            $row = Order_item::where('order_id', $exist->id)->where('item_id', $request->item_id)->first();
+            if ($row) {
+                $row->update(['quantity' => $row->quantity + 1]);
+            } else {
+                $newItem = new Order_item();
+
+                $newItem->order_id = $exist->id;
+                $newItem->item_id = $request->item_id;
+                $newItem->quantity = 1;
+                $newItem->save();
+            }
+            return $this->sendResponse(null, ' Cart updated successfully.');
+        } else {
+//create order & details
+try
+{
+    // Disable foreign key checks!
+    DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+            $newOrder = new Order();
+
+            $newOrder->order_date = Carbon::parse(now());
+            $newOrder->status_id = 0;
+            $newOrder->user_id = $user->id;
+            $newOrder->save();
+
+            $newItem = new Order_item();
+
+            $newItem->order_id = $newOrder->id;
+            $newItem->item_id = $request->item_id;
+            $newItem->quantity = 1;
+            $newItem->save();
+            DB::commit();
+            // Enable foreign key checks!
+            DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+            // Display a successful message ...
+            return $this->sendResponse(null, ' Cart updated successfully.');
+        } catch (\Exception$e) {
+            DB::rollback();
+            return $this->sendError($e->getMessage(), 'Error happens!!');
+        }
+        }
+        if ($validator->fails()) {
+            return $this->convertErrorsToString($validator->messages());
+        }
+
+
+    }
     public function AddQuantity($id)
     {
         $row = Order_item::where('id', $id)->first();
@@ -46,8 +105,8 @@ class CartController extends BaseController
         return $this->sendResponse($row, ' Cart updated successfully.');
     }
 
-
-    public function placeOrder(Request $request){
+    public function placeOrder(Request $request)
+    {
 
     }
 }
